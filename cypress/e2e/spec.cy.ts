942082ff5a43eaccade03cd0abe26b0377eaa1df
Aliaksei Olesik <AOlesik@alfabank.ru>
2025-02-11 13:13:19 +0300

    feat(*): update screenshot fn

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,11 +1,31 @@
 /// <reference types="cypress" />
 
 describe('App Component', () => {
+    const resolutionFull = [
+        { width: 360, height: 740 },
+        { width: 833, height: 900 },
+        { width: 1440, height: 900 },
+    ];
+    // const resolutionMobile = [{ width: 360, height: 740 }];
+    // const resolutionTablet = [{ width: 833, height: 900 }];
+    // const resolutionLaptop = [{ width: 1440, height: 900 }];
+    const resolutionDesctop = [{ width: 1920, height: 1080 }];
+
+    function takeScreenshots(screenshotName: string, resolution = resolutionFull) {
+        cy.wait(1000);
+        for (let i = 0; i < resolution.length; i++) {
+            cy.viewport(resolution[i].width, resolution[i].height);
+            cy.screenshot(`${screenshotName}_${resolution[i].width}x${resolution[i].height}`, {
+                capture: 'viewport',
+            });
+        }
+    }
+
     beforeEach(() => {
         cy.visit('http://localhost:3000');
     });
 
     it('should take a screenshot of the app', () => {
-        cy.screenshot('app-screenshot');
+        takeScreenshots('main-page', resolutionDesctop);
     });
 });

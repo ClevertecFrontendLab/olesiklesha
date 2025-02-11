1f861f26ed02320569717e6727395257b573e9e8
Aliaksei Olesik <AOlesik@alfabank.ru>
2025-02-11 13:44:05 +0300

    feat(*): update path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [react()],
+    base: command === 'build' ? '/olesiklesha/' : '/',
     server: {
         host: true,
         port: 3000,
@@ -13,4 +14,4 @@ export default defineConfig({
             '@public': path.resolve(__dirname, 'public'),
         },
     },
-});
+}));
